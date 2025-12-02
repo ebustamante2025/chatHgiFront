830c1b5bacfe633ccc1c55203a770f5be6346e07
ebustamante2025 <ebustamante@hgi.com.co>
2025-12-02 16:52:31 -0500

    actualizacion rutas htt

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 //const API_URL = "http://localhost:4000";
 //const API_URL = "https://chat.hginet.com.co:4000"
-const API_URL = "https://chat.hginet.com.co/api";
+const API_URL = "https://chat.hginet.com.co";
 
 // Registramos usuarios
 export async function register(username, password) {
